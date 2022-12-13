fc3cf7a09c352092d5292704666afc904b2bc63e
vladislav001015 <vladislav001015@gmail.com>
2022-12-13 18:58:27 +0600

    added new logic with likes and image

--- applications/post/admin.py
+++ applications/post/admin.py
@@ -11,9 +11,14 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [
         ImageAdmin
     ]
+    list_display = ['id', 'title', 'post_count_like']
+
+    def post_count_like(self, obj):
+        return obj.likes.filter(like=True).count()
 
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Like)
 admin.site.register(Rating)
+admin.site.register(Image)
--- applications/post/serializers.py
+++ applications/post/serializers.py
@@ -29,13 +29,11 @@ class PostSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         request = self.context.get('request')
-        images_data = request.FILES
-        print(images_data)
+        files_data = request.FILES
         post = Post.objects.create(**validated_data)
 
-        for image in images_data.getlist('images'):
-            pass
-        # TODO: сохранять картинки и в админке выводить количество лайков в каждому посту
+        for image in files_data.getlist('images'):
+            Image.objects.create(post=post, image=image)
 
         return post
 
